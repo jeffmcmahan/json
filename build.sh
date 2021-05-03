7ae2a9c39cdc4bf8209e49e32a3b1ec5b796e9f7
Jeff McMahan <jeffrey.lee.mcmahan@gmail.com>
2021-05-03 16:30:53 -0400

    managed the /assembly dir's creation/deletion

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
-# BROWSER JS BUILD
-# Build sourcemap, no --minify, and include tests.
+# DENO: import {json} from 'src/main.ts'
+# NODE: import {json} from 'dist/json.dev.mjs'
+
+# BROWSER JS BUILDS
+# <script type="module" src="[...]/json.dev.mjs"></script>
+# Dev build - sourcemap, no minification, include tests.
 cd src
 ../node_modules/.bin/esbuild main.src.ts \
 	--bundle \
@@ -17,13 +21,25 @@ cd src
 #	--format=esm \
 # --outfile=../dist/json.mjs
 
-# WASM BUILD
+# WASM BUILDS
+# Dev build - include tests
 cd ../
-rm -r ./wasm/assembly/**/*
+rm -r ./wasm/assembly
+mkdir ./wasm/assembly
 cp -r ./src/. ./wasm/assembly/
 cd wasm/assembly && rm -rf .git
 cd wasm/assembly
 mv main.ts index.ts
-npm run asbuild
-# ^^ fails because of explicit deno-style ".ts" imports
-# @todo - strip the file extensions after copying
+npm run asbuild # fails because of explicit deno-style ".ts" imports
+rm -r ./wasm/assembly
+
+# Production build
+# cd ../
+# rm -r ./wasm/assembly
+# mkdir ./wasm/assembly
+# cd ./tmp (... where the .src files are gone)
+# cp -r ./src/. ./wasm/assembly/
+# ... &c.
+# npm run asbuild
+# rm -r ./wasm/assembly
+# cd ../ && rm -r ./tmp
